63317f11bcf2c852cb3bcdc321cac15985979e49
hongchen.swl <hongchen.swl@bytedance.com>
2023-05-18 13:12:09 +0800

    feat(hc): add openai request

--- xxn.js
+++ xxn.js
@@ -6,22 +6,25 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 
-const request = async () => {
+const requestXXN = async (content) => {
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [
-            {role: "system", content: "你是小红书爆款写作专家，每次创作，首先产出 5 个标题（含适当的 emoji 表情），其次产出 1 个正文（每一个段落含有适当的 emoji 表情，文末有合适的 tag 标签）"},
+            {
+                role: "system",
+                content: "你是小红书爆款写作专家，每次创作，首先产出 5 个标题（含适当的 emoji 表情），其次产出 1 个正文（每一个段落含有适当的 emoji 表情，文末有合适的 tag 标签）"
+            },
             {
                 role: "user",
-                content: "帮我写一篇牛肉面相关的爆款文章"
+                content: content
             }
         ],
         user: "hongchen123"
     });
     console.log(completion.data.choices[0].message);
-    return completion
+    return completion.data.choices[0].message
 }
 
 
-request()
+module.exports = requestXXN
 
